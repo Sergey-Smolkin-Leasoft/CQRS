41850dc662d17d4d63a7a469c7aa4a57930ffd30
Sergey-Smolkin-Leasoft <143398413+Sergey-Smolkin-Leasoft@users.noreply.github.com>
2024-05-15 22:07:33 +0300

    Init

---
+++ app/src/CQRS/CommandBusInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\CQRS;
+
+interface CommandBusInterface 
+{
+
+    public function dispatch(CommandInterface $command): void;
+
+    public function dispatchMany();
+}
