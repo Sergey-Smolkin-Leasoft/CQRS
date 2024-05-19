05c121794992ade2e197c6f4bf769b2adc0c7d62
Sergey-Smolkin-Leasoft <143398413+Sergey-Smolkin-Leasoft@users.noreply.github.com>
2024-05-19 17:39:02 +0300

    wip

--- app/src/CQRS/AttributesHandlersLocators.php
+++ app/src/CQRS/AttributesHandlersLocators.php
@@ -4,14 +4,70 @@ declare(strict_types=1);
 
 namespace App\CQRS;
 
+use App\CQRS\Attributes\CommandHandler;
+use Spiral\Core\Attribute\Singleton;
+use Spiral\Tokenizer\Attribute\TargetAttribute;
+use Spiral\Tokenizer\TokenizationListenerInterface;
 use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Handler\HandlerDescriptor;
 use Symfony\Component\Messenger\Handler\HandlersLocatorInterface;
 
-final class AttributesHandlersLocators implements HandlersLocatorInterface
+#[Singleton]
+#[TargetAttribute(CommandHandler::class)]
+final class AttributesHandlersLocators implements
+    HandlersLocatorInterface,
+    HandlersRegistryInterface,
+    TokenizationListenerInterface
 {
+    private array $handlers = [];
 
     public function getHandlers(Envelope $envelope): iterable
     {
-        // TODO: Implement getHandlers() method.
+        foreach ($this->handlers as $commandClass => $handlers) {
+            if( !$envelope->getMessage() instanceof $commandClass) {
+                continue;
+            }
+
+            foreach ($handlers as $handler) {
+                yield new HandlerDescriptor($handler);
+            }
+        }
+    }
+
+    /**
+     * @param class-string<CommandInterface> $commandClass
+     *
+     * @return void
+     */
+    public function register(string $commandClass, callable $handler): void
+    {
+        $this->handlers[$commandClass][] = $handler;
+    }
+
+    /**
+     * The method will be fired for each class that was found by Tokenizer.
+     */
+    public function listen(\ReflectionClass $class): void
+    {
+        if (!$class->isInstantiable()) {
+            return;
+        }
+
+        foreach ($class->getMethods() as $method) {
+            if (!$method->isPublic()) {
+                continue;
+            }
+
+            $this->registerCommandHandler($method);
+            $this->registerQueryHandler($method);
+        }
+    }
+
+    /**
+     * The method will be fired after all classes were processed by listen method.
+     */
+    public function finalize(): void
+    {
+        // TODO: Implement finalize() method.
     }
 }
