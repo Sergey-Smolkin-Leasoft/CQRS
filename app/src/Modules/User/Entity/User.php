0c6ed0e3a7762d7a3902e2990b88730ce0ff04e7
Sergey-Smolkin-Leasoft <143398413+Sergey-Smolkin-Leasoft@users.noreply.github.com>
2024-05-23 21:48:10 +0300

    user entity

---
+++ app/src/Modules/User/Entity/User.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\User\Entity;
+
+class User {
+    public function __construct(
+        public string $id,
+        public string $name,
+        public string $email,
+        public string $password,
+        public bool $isActive = false,
+    ) {}
+}
